9f21489de52f9ce3290116327896d837ac868b1e
nawthtech <limb-unbent-busily@duck.com>
2025-11-26 19:29:22 +0400

    Create store_service.go

---
+++ backend/internal/services/store_service.go
@@ -0,0 +1,9 @@
+// internal/services/store_service.go
+package services
+
+// StoreService يقدم خدمات إدارة المتجر
+type StoreService struct{}
+
+func NewStoreService() *StoreService {
+	return &StoreService{}
+}
