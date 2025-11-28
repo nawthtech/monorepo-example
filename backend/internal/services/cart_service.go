36c64eb1522407f9944cc898f53c9da7864353a8
nawthtech <limb-unbent-busily@duck.com>
2025-11-28 07:59:24 +0400

    Delete backend/internal/services/cart_service.go

--- backend/internal/services/cart_service.go
+++
@@ -1,9 +0,0 @@
-// internal/services/cart_service.go
-package services
-
-// CartService يقدم خدمات إدارة السلة
-type CartService struct{}
-
-func NewCartService() *CartService {
-	return &CartService{}
-}
