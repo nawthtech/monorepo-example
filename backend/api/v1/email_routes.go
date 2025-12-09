7887b7e3ab38c3f546c397d79c6fe0382ba3d7b6
nawthtech <limb-unbent-busily@duck.com>
2025-12-09 10:54:32 +0400

    Create email_routes.go

---
+++ backend/api/v1/email_routes.go
@@ -0,0 +1,24 @@
+// backend/api/v1/email_routes.go
+package v1
+
+import (
+	"github.com/gin-gonic/gin"
+	"github.com/nawthtech/backend/internal/handlers"
+)
+
+func RegisterEmailRoutes(router *gin.RouterGroup) {
+	emailHandler, err := handlers.NewEmailHandler()
+	if err != nil {
+		// Log error but don't crash
+		return
+	}
+	
+	emailGroup := router.Group("/email")
+	{
+		emailGroup.POST("/setup", emailHandler.SetupEmail)
+		emailGroup.GET("/allow-list", emailHandler.GetAllowList)
+		emailGroup.POST("/allow-list", emailHandler.AddToAllowList)
+		emailGroup.DELETE("/allow-list/:email", emailHandler.RemoveFromAllowList)
+		emailGroup.POST("/test", emailHandler.TestEmail)
+	}
+}
